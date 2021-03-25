373844c8c2713525c4c54f62a659d366879cd724
Priston506 <57559457+Priston506@users.noreply.github.com>
2021-03-25 15:05:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function time(){
-    var d = new Date();
-    var hours = d.getHours();
-    var minutes = d.getMinutes();
-    var seconds = d.getUTCSeconds();
-    document.getElementById('clock').innerHTML = hours + ":" + minutes + ":" + seconds;
+function clock(){
+    let hour = document.getElementById('hour');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+    let ampm = document.getElementById('ampm');
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    var am = "AM";
+
+    if(h > 12){
+        h = h -12;
+        var am = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    hour.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+    ampm.innerHTML = am;
 }
 
-setInterval(time , 1000);
+var interval = setInterval(clock, 1000);
 
 
